2a8d7e7d0935c1106728d1afb15c4a7917c7daa7
daniel-certa-1228 <mfdancerta@gmail.com>
2017-11-09 09:51:12 -0600

    initial commit

---
+++ century.rb
@@ -0,0 +1,13 @@
+def centuryFromYear(year)
+
+    if year == 0
+        0
+    elsif year.between?(1,100)
+        1
+    else
+        year = (year/100) + 1
+        return year
+    end
+    
+end
+    
---
+++ century_spec.rb
@@ -0,0 +1,32 @@
+require_relative 'century.rb'
+
+describe "centuryFromYear" do
+    context 'when year = 0' do
+        it "returns 0" do
+            zero = centuryFromYear(0)
+            expect(zero).to eq(0)
+        end
+    end
+    context 'when year is between 1 and 100' do
+        it "returns 1" do
+            test1 = centuryFromYear(5)
+            expect(test1).to eq(1)
+            test2 = centuryFromYear(20)
+            expect(test2).to eq(1)
+            test3 = centuryFromYear(100)
+            expect(test3).to eq(1)
+            test4 = centuryFromYear(101)
+            expect(test4).not_to eq(1)
+        end
+    end
+    context 'when year is above 100' do
+        it "returns the correct century" do
+            test1 = centuryFromYear(101)
+            expect(test1).to eq(2)
+            test2 = centuryFromYear(201)
+            expect(test2).to eq(3)
+            test3 = centuryFromYear(301)
+            expect(test3).to eq(4)
+        end
+    end
+end
